b6a0595335d75bc2fa704282629e50daabd651da
sachin19927 <sachin19927@gmail.com>
2019-11-30 18:42:26 +0530

    Initial commit

---
+++ src/com/java/jdbc/TestJdbc.java
@@ -0,0 +1,27 @@
+package com.java.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	
+	public static void main(String[] args) {
+		
+		String jdbcUrl="jdbc:postgresql://localhost:5432/personal";
+		//String jdbcUrl="jdbc:postgresql://localhost:5432/personal?useSSL=false"; // to bypass SSL warings
+		String userName="postgres";
+		String passWord="sachin";
+		try
+		{
+			System.err.println("Connecting Data base: "+ jdbcUrl);
+			Connection con=DriverManager.getConnection(jdbcUrl, userName, passWord);
+			System.err.println("Connection Successful");
+		}
+		catch (Exception e) {
+		System.err.println(e.getMessage());
+		}
+		
+		
+	}
+}
